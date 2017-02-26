6137ceb3b7d5fbce0e000da491c254db3d6f4b98
jefflunt <jefflunt@gmail.com>
2017-02-26 00:05:41 -0600

    Fixed bot interface. Confirmed that it actually runs now.

--- lib/bot_interface.rb
+++ lib/bot_interface.rb
@@ -1,3 +1,5 @@
+require 'attack'
+
 ##
 # This module is your interface to writing a custom bot, and except in the case
 # of a bug in the main library, it should be the only code you need to modify to
@@ -8,25 +10,33 @@
 module BotInterface
   ##
   # Called in response to http://dev.generals.io/api#game_start
+  #
+  # NOTE: This method's return value is ignored
   def self.start(game_state)
-    # This method's return value is ignored
+    Logger.log "BOT just sitting here"
   end
 
   ##
   # Called in response to http://dev.generals.io/api#game_update
   #
   # game_state - a fully patched GameState instance. See lib/game_state.rb
+  #
+  # This method return value should be either:
+  # - nil
+  # - an instance of the Attack class (see lib/attack.rb)
+  #
+  # Any non-nil value that isn't an Attack class will cause the client to crash.
   def self.update(game_state)
-    # This method return value should be either:
-    # - nil
-    # - an instance of the Attack class (see lib/attack.rb)
+    Logger.log "BOT just sitting here"
   end
 
   ##
   # Called in response to either to:
   # - http://dev.generals.io/api#game_lost
   # - http://dev.generals.io/api#game_won
+  #
+  # This method's return value is ignored
   def self.game_over(game_state)
-    # This method's return value is ignored
+    Logger.log "BOT just sitting here"
   end
 end
